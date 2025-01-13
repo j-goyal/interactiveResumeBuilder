33c7c2b358e30b778b76b7411b8e28ae19025eab
j-goyal <jating07925@gmail.com>
2025-01-13 17:53:15 +0530

    ProperFavicon

--- app/utils/validationSchema.ts
+++ app/utils/validationSchema.ts
@@ -5,7 +5,7 @@ export const resumeValidationSchema = Yup.object({
   email: Yup.string().email('Invalid email address').required('Email is required'),
   phone: Yup.string()
     .matches(
-      /^(?:\+91|91|0)?\d{10}$/, 
+      /^(?:\+91\s?|91\s?|0)?\d{10}$/, 
       'Invalid phone number (Indian phone numbers should have 10 digits and may start with +91, 91, or 0)'
     )
     .required('Phone is required'),
